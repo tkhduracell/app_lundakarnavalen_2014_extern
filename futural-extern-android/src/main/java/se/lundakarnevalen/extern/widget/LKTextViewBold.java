5ba4cd8ff8310ec7983a99a100c9aa61d3aac740
atp10mla <atp10mla@student.lu.se>
2014-04-14 18:41:01 +0200

    Update text in bottom menu

---
+++ futural-extern-android/src/main/java/se/lundakarnevalen/extern/widget/LKTextViewBold.java
@@ -0,0 +1,39 @@
+package se.lundakarnevalen.extern.widget;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Custom textview with custom font. 
+ *
+ */
+public class LKTextViewBold extends TextView {
+    Context context;
+
+    public LKTextViewBold(Context context) {
+        super(context);
+        this.context = context;
+        font();
+
+    }
+
+    public LKTextViewBold(Context context, AttributeSet attrs){
+        super(context, attrs);
+        this.context = context;
+        font();
+    }
+
+    public LKTextViewBold(Context context, AttributeSet attrs, int defStyle){
+        super(context, attrs, defStyle);
+        this.context = context;
+        font();
+    }
+
+    private void font(){
+
+        Typeface tf = Typeface.createFromAsset(context.getAssets(), "fonts/Futura-Bold.ttf");
+        setTypeface(tf);
+    }
+}
