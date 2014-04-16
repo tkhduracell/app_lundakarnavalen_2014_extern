6e0903c63086671c0b9dfc4178fdb5dd65001d1e
atp10mla <atp10mla@student.lu.se>
2014-04-16 09:24:19 +0200

    Add fragments

---
+++ futural-extern-android/src/main/java/fragments/SchemeFragment.java
@@ -0,0 +1,31 @@
+package fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import se.lundakarnevalen.extern.android.R;
+
+/**
+ * Created by Markus on 2014-04-16.
+ */
+public class SchemeFragment extends LKFragment{
+
+
+    // Every time you switch to this fragment.
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_scheme, null);
+
+        return rootView;
+    }
+
+
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setTitle(getString(R.string.scheme));
+    }
+}
