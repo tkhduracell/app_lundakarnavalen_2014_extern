ea76d556cf3c5c49569f263fcc870a5109bec7b9
Markus <karl.markus.larsson@gmail.com>
2014-04-16 17:38:48 +0200

    add information about fun in strings

--- futural-extern-android/src/main/java/se/lundakarnevalen/extern/android/SplashScreenActivity.java
+++ futural-extern-android/src/main/java/se/lundakarnevalen/extern/android/SplashScreenActivity.java
@@ -3,18 +3,15 @@ package se.lundakarnevalen.extern.android;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Handler;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class SplashSceenActivity extends Activity {
+public class SplashScreenActivity extends Activity {
     private static int SPLASH_TIME_OUT = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_sceen);
+        setContentView(R.layout.activity_splash_screen);
         new Handler().postDelayed(new Runnable() {
 
             /*
@@ -26,7 +23,7 @@ public class SplashSceenActivity extends Activity {
             public void run() {
                 // This method will be executed once the timer is over
                 // Start your app main activity
-                Intent i = new Intent(SplashSceenActivity.this, ContentActivity.class);
+                Intent i = new Intent(SplashScreenActivity.this, ContentActivity.class);
                 startActivity(i);
                 // close this activity
                 finish();
