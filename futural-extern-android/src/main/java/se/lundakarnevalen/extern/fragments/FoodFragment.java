8deef60c6feef2e4b96bacb15e5c28aa6f4b73f1
Filip Lindqvist <buggfille@gmail.com>
2014-04-18 20:54:57 +0200

    Added dummy implementation of Food and fun Fragment

--- futural-extern-android/src/main/java/se/lundakarnevalen/extern/fragments/FoodFragment.java
+++ futural-extern-android/src/main/java/se/lundakarnevalen/extern/fragments/FoodFragment.java
@@ -4,6 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import se.lundakarnevalen.extern.android.R;
 
@@ -17,7 +23,22 @@ public class FoodFragment extends LKFragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_food, null);
+        ListView lv = (ListView) rootView.findViewById(R.id.fragment_food_list);
 
+        lv.setAdapter(new SimpleAdapter(getContext(),
+                new ArrayList<Map<String, String>>(){{
+                    add(new HashMap<String, String>() {{
+                        put("title","Fine Dine");
+                        put("plats", "Område: D7");
+                    }});
+                    add(new HashMap<String, String>() {{
+                        put("title","KarneKörv!");
+                        put("plats", "Område: F3");
+                    }});
+                }}, R.layout.element_listitem,
+                new String[]{ "title", "plats" },
+                new int[]{ android.R.id.text1, android.R.id.text2 }
+        ));
         return rootView;
     }
 
