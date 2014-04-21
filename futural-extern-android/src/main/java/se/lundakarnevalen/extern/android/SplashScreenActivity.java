e1788ddf543cd0e3779d227478ce4d68e39ab1ea
Filip Lindqvist <buggfille@gmail.com>
2014-04-21 18:11:35 +0200

    Fancy splashscreen

--- futural-extern-android/src/main/java/se/lundakarnevalen/extern/android/SplashScreenActivity.java
+++ futural-extern-android/src/main/java/se/lundakarnevalen/extern/android/SplashScreenActivity.java
@@ -2,34 +2,44 @@ package se.lundakarnevalen.extern.android;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Handler;
 import android.os.Bundle;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.ImageView;
+
+import static se.lundakarnevalen.extern.util.ViewUtil.*;
 
 public class SplashScreenActivity extends Activity {
-    private static int SPLASH_TIME_OUT = 200;
+    private static final int SPLASH_TIME_OUT = 300;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
         new Handler().postDelayed(new Runnable() {
-
-            /*
-             * Showing splash screen with a timer. This will be useful when you
-             * want to show case your app logo / company
-             */
-
             @Override
             public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
                 Intent i = new Intent(SplashScreenActivity.this, ContentActivity.class);
                 startActivity(i);
-                // close this activity
+                if(Build.VERSION.SDK_INT >= 11) {
+                    overridePendingTransition(R.anim.abc_fade_in, R.anim.abc_fade_out);
+                }
                 finish();
             }
         }, SPLASH_TIME_OUT);
+
+        if(Build.VERSION.SDK_INT >= 11){
+            find(R.id.imgLogo, ImageView.class).animate()
+                .rotationBy(360)
+                .setDuration(SPLASH_TIME_OUT)
+                .setInterpolator(new AccelerateDecelerateInterpolator())
+                .start();
+        }
     }
 
-
+    public <T> T find(int id, Class<T> clz) {
+        return clz.cast(findViewById(id));
+    }
 }
