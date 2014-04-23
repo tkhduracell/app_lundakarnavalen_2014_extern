bc21a8aadb7bc563cac5cfb6cc183861a58aa890
atp10mla <atp10mla@student.lu.se>
2014-04-23 19:09:46 +0200

    update design landingpage

--- futural-extern-android/src/main/java/se/lundakarnevalen/extern/fragments/OtherFragment.java
+++ futural-extern-android/src/main/java/se/lundakarnevalen/extern/fragments/OtherFragment.java
@@ -16,6 +16,8 @@ import se.lundakarnevalen.extern.android.ContentActivity;
 import se.lundakarnevalen.extern.android.R;
 import se.lundakarnevalen.extern.widget.LKListAdapter;
 import se.lundakarnevalen.extern.widget.LKListElement;
+import se.lundakarnevalen.extern.widget.LKListElementType;
+import se.lundakarnevalen.extern.widget.LKListRow;
 
 /**
  * Created by Markus on 2014-04-16.
@@ -34,13 +36,13 @@ public class OtherFragment extends LKFragment{
         }
 
 
-        ArrayList<LKListAdapter.LKListRow> items = new ArrayList<LKListAdapter.LKListRow>();
+        ArrayList<LKListRow> items = new ArrayList<LKListRow>();
 
         for(int i = 0;i< other.size();i=i+2){
             if(i+1<other.size()) {
-                items.add(new LKListAdapter.LKListRow(other.get(i),other.get(i+1)));
+                items.add(new LKListRow(other.get(i),other.get(i+1)));
             } else {
-                items.add(new LKListAdapter.LKListRow(other.get(i),null));
+                items.add(new LKListRow(other.get(i),null));
             }
 
         }
@@ -52,71 +54,66 @@ public class OtherFragment extends LKFragment{
 
 
     private void addAllOther() {
-        LKListElement music = new LKListElement(
+        other.add(new LKListElement(
                 "",
                 getString(R.string.radio_title),
                 "",
                 0, 0,
                 0,
                 R.drawable.radio_logo,
-                "");
-        music.isMusic = true;
-        other.add(music);
+                "",
+                LKListElementType.RADIO));
 
-        LKListElement otherElement = new LKListElement(
+        other.add(new LKListElement(
                 "",
                 getString(R.string.security_title),
                 "",
                 0, 0,
                 0,
                 R.drawable.security_logo,
-                "");
-        otherElement.isOther = true;
-        other.add(otherElement);
+                "",
+                LKListElementType.OTHER));
 
-        otherElement = new LKListElement(
+        other.add(new LKListElement(
                 "",
                 getString(R.string.atm_title),
                 "",
                 0, 0,
                 0,
                 R.drawable.atm_logo,
-                "");
-        otherElement.isOther = true;
-        other.add(otherElement);
+                "",
+                LKListElementType.OTHER));
 
-        otherElement = new LKListElement(
+
+        other.add(new LKListElement(
                 "",
                 getString(R.string.parking_title),
                 "",
                 0, 0,
                 0,
                 R.drawable.parking_logo,
-                "");
-        otherElement.isOther = true;
-        other.add(otherElement);
+                "",
+                LKListElementType.OTHER));
 
-        otherElement = new LKListElement(
+        other.add(new LKListElement(
                 "",
                 getString(R.string.care_title),
                 "",
                 0, 0,
                 0,
                 R.drawable.health_logo,
-                "");
-        otherElement.isOther = true;
-        other.add(otherElement);
+                "",
+                LKListElementType.OTHER));
 
-        otherElement = new LKListElement(
+        other.add(new LKListElement(
                 "",
                 getString(R.string.toilets_title),
                 "",
                 0, 0,
                 0,
                 R.drawable.toilets_logo,
-                "");
-        otherElement.isOther = true;
-        other.add(otherElement);
+                "",
+                LKListElementType.OTHER));
     }
 
     @Override
---
+++ futural-extern-android/src/main/java/se/lundakarnevalen/extern/widget/LKListElementType.java
@@ -0,0 +1,11 @@
+package se.lundakarnevalen.extern.widget;
+
+/**
+ * Created by Markus on 2014-04-23.
+ */
+public class LKListElementType {
+    public static final int FOOD = 1;
+    public static final int FUN = 2;
+    public static final int OTHER = 3;
+    public static final int RADIO = 4;
+}
