6f0660900c20ca40a8a0463c83adbb6da7d0d5e0
Markus <karl.markus.larsson@gmail.com>
2014-04-23 13:53:54 +0200

    update other layout

--- futural-extern-android/src/main/java/se/lundakarnevalen/extern/fragments/OtherFragment.java
+++ futural-extern-android/src/main/java/se/lundakarnevalen/extern/fragments/OtherFragment.java
@@ -14,11 +14,14 @@ import java.util.Map;
 
 import se.lundakarnevalen.extern.android.ContentActivity;
 import se.lundakarnevalen.extern.android.R;
+import se.lundakarnevalen.extern.widget.LKListAdapter;
+import se.lundakarnevalen.extern.widget.LKListElement;
 
 /**
  * Created by Markus on 2014-04-16.
  */
 public class OtherFragment extends LKFragment{
+    private ArrayList<LKListElement> other = new ArrayList<LKListElement>();
 
 
     // Every time you switch to this fragment.
@@ -26,45 +29,95 @@ public class OtherFragment extends LKFragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_other, null);
         ListView lv = (ListView) rootView.findViewById(R.id.fragment_other_list);
-        addElementsToOther();
-        lv.setAdapter(new SimpleAdapter(getContext(),
-            new ArrayList<Map<String, String>>(){{
-                add(new HashMap<String, String>(){{
-                    put("title","Music");
-                    put("time", "17:00 - 19:00");
-                }});
-                add(new HashMap<String, String>(){{
-                    put("title","OtherStuff");
-                    put("time", "21:00 - 23:00");
-                }});
-            }}, R.layout.element_listitem,
-            new String[]{ "title", "time" },
-            new int[]{ android.R.id.text1, android.R.id.text2 }
-        ));
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                ContentActivity contentActivity = ContentActivity.class
-                        .cast(getActivity());
-                if(position == 0) {
-                    contentActivity
-                        .loadFragmentWithAdd(new MusicFragment());
-                } else {
-                    contentActivity
-                        .loadFragmentWithAdd(
-                            LandingPageFragment.create(
-                                "Filip", "Lindqvist",
-                                false, true, true,
-                                56.055876056f, 12.9737800f,
-                                R.drawable.monk, R.drawable.monk,
-                                "VAD ÄR FILIP BRA PÅ?", "ÄTA KEBAB!"));
-                }
+        if(other.isEmpty()) {
+            addAllOther();
+        }
+
+
+        ArrayList<LKListAdapter.LKListRow> items = new ArrayList<LKListAdapter.LKListRow>();
+
+        for(int i = 0;i< other.size();i=i+2){
+            if(i+1<other.size()) {
+                items.add(new LKListAdapter.LKListRow(other.get(i),other.get(i+1)));
+            } else {
+                items.add(new LKListAdapter.LKListRow(other.get(i),null));
             }
-        });
+
+        }
+        lv.setAdapter(new LKListAdapter(getContext(), items,getActivity()));
+
+
         return rootView;
     }
 
-    private void addElementsToOther() {}
+
+    private void addAllOther() {
+        LKListElement music = new LKListElement(
+                "",
+                getString(R.string.radio_title),
+                "",
+                0, 0,
+                0,
+                R.drawable.radio_logo,
+                "");
+        music.isMusic = true;
+        other.add(music);
+
+        LKListElement otherElement = new LKListElement(
+                "",
+                getString(R.string.security_title),
+                "",
+                0, 0,
+                0,
+                R.drawable.security_logo,
+                "");
+        otherElement.isOther = true;
+        other.add(otherElement);
+
+        otherElement = new LKListElement(
+                "",
+                getString(R.string.atm_title),
+                "",
+                0, 0,
+                0,
+                R.drawable.atm_logo,
+                "");
+        otherElement.isOther = true;
+        other.add(otherElement);
+
+        otherElement = new LKListElement(
+                "",
+                getString(R.string.parking_title),
+                "",
+                0, 0,
+                0,
+                R.drawable.parking_logo,
+                "");
+        otherElement.isOther = true;
+        other.add(otherElement);
+
+        otherElement = new LKListElement(
+                "",
+                getString(R.string.care_title),
+                "",
+                0, 0,
+                0,
+                R.drawable.health_logo,
+                "");
+        otherElement.isOther = true;
+        other.add(otherElement);
+
+        otherElement = new LKListElement(
+                "",
+                getString(R.string.toilets_title),
+                "",
+                0, 0,
+                0,
+                R.drawable.toilets_logo,
+                "");
+        otherElement.isOther = true;
+        other.add(otherElement);
+    }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
