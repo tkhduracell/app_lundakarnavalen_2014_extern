9d082081e60e07481ef33af6b70d3e513de75523
Filip Lindqvist <buggfille@gmail.com>
2014-04-24 15:42:09 +0200

    Removed static cache, reimplemented scaling using GestureDetectors

--- futural-extern-android/src/main/java/se/lundakarnevalen/extern/fragments/MapFragment.java
+++ futural-extern-android/src/main/java/se/lundakarnevalen/extern/fragments/MapFragment.java
@@ -68,28 +68,26 @@ public class MapFragment extends LKFragment {
         }
 
         img = ((SVGMapView) rootView.findViewById(R.id.map_id));
-        if(staticCache == null){
-            new AsyncTask<Void, Void, Void>(){
-                @Override
-                protected Void doInBackground(Void... params) {
-                    try {
-                        Timer t = new Timer();
-                        SVG svg = staticCache = SVG.getFromResource(inflater.getContext(), R.raw.map3);
-                        img.setSvg(svg, imageWidth, imageHeight, dip);
-                        t.tick(LOG_TAG, "getFromResource()");
-                    } catch (SVGParseException e) {
-                        Log.wtf(LOG_TAG, "This wont happen");
-                    }
-                    return null;
+
+        new AsyncTask<Void, Void, Void>(){
+            @Override
+            protected Void doInBackground(Void... params) {
+                try {
+                    Timer t = new Timer();
+                    SVG svg = SVG.getFromResource(inflater.getContext(), R.raw.map3);
+                    img.setSvg(svg, imageWidth, imageHeight, dip);
+                    t.tick(LOG_TAG, "getFromResource()");
+                } catch (SVGParseException e) {
+                    Log.wtf(LOG_TAG, "This wont happen");
                 }
-            }.execute();
-        } else {
-            img.setSvg(staticCache, imageWidth, imageHeight, dip);
-        }
+                return null;
+            }
+        }.execute();
+
 
         if(savedInstanceState != null && savedInstanceState.containsKey(STATE_MATRIX)){
             Log.d(LOG_TAG, "Matrix values restored");
-            img.setMatrixValues(savedInstanceState.getFloatArray(STATE_MATRIX));
+            //img.setMatrixValues(savedInstanceState.getFloatArray(STATE_MATRIX));
         }
 
         setRetainInstance(true);
@@ -101,7 +99,7 @@ public class MapFragment extends LKFragment {
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         Log.d(LOG_TAG, "onSaveInstanceState() called");
-        outState.putFloatArray(STATE_MATRIX, img.getMatrixValues());
+        //outState.putFloatArray(STATE_MATRIX, img.getMatrixValues());
     }
 
     public static MapFragment create(boolean zoom, float lat, float lng) {
--- futural-extern-android/src/main/java/se/lundakarnevalen/extern/widget/SVGMapView.java
+++ futural-extern-android/src/main/java/se/lundakarnevalen/extern/widget/SVGMapView.java
@@ -11,172 +11,168 @@ import android.graphics.PorterDuff;
 import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.caverock.androidsvg.SVG;
 
+import se.lundakarnevalen.extern.util.Logf;
 import se.lundakarnevalen.extern.util.Timer;
 
 /**
  * Created by Filip on 2014-04-23.
  */
-public class SVGMapView extends View implements View.OnTouchListener {
+public class SVGMapView extends View {
 
     private static final String LOG_TAG = SVGMapView.class.getSimpleName();
-    private boolean firstTime;
+    private static final int INVALID_POINTER_ID = -1;
 
     private Matrix matrix;
-    private Matrix savedMatrix;
 
-    // Variables for zooming
-    private PointF start = new PointF();
-    private PointF mid = new PointF();
-    private float oldDist = 1f;
-    private float newDist = 1f;
-
-    // Control scale 1 = full size
-    private float scale = 1f;
-
-    // States onTouchEvent
-    private final int NONE = 0;
-    private int mode = NONE;
-    private final int DRAG = 1;
-    private final int ZOOM = 2;
+    private float mScaleFactor;
 
     private SVG svg = null;
+
     private Rect r = new Rect();
     private Picture pic = new Picture();
     private Timer timer = new Timer();
+    private ScaleGestureDetector mScaleDetector;
+    private GestureDetector mGestures;
+
+    private float mPosX;
+    private float mPosY;
+
+    private float mLastTouchX;
+    private float mLastTouchY;
+    // The focus point for the scaling
+    private float scalePointX;
+    private float scalePointY;
+
+    private int mActivePointerId;
+    private float lastFocusY;
+    private float lastFocusX;
+
 
     public SVGMapView(Context context) {
         super(context);
-        init();
+        init(context);
     }
 
     public SVGMapView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init();
+        init(context);
     }
 
     public SVGMapView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        init();
+        init(context);
     }
 
-    public void init(){
-        matrix = savedMatrix = new Matrix();
-        firstTime = true;
-        setOnTouchListener(this);
+    public void init(Context context){
+        matrix = new Matrix();
+        mScaleDetector = new ScaleGestureDetector(context, new ScaleListener());
+        mGestures = new GestureDetector(getContext(), new GestureListener());
     }
-
 
     @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        firstTime = false;
-        float scale;
-
-        // Handle touch events here...
-        switch (event.getAction() & MotionEvent.ACTION_MASK) {
-            case MotionEvent.ACTION_DOWN:   // first finger down only
-                savedMatrix.set(matrix);
-                start.set(event.getX(), event.getY());
-                mode = DRAG;
-                break;
-
-            case MotionEvent.ACTION_UP: // first finger lifted
-
-            case MotionEvent.ACTION_POINTER_UP: // second finger lifted
-                if (mode == ZOOM) {
-                    this.scale = this.scale * newDist / oldDist;
-                    // generateDots(this.scale);
-                } else {
-
-                    if ((start.x - event.getX()) * (start.x - event.getX()) + (start.y - event.getY()) * (start.y - event.getY()) < 10) {
-                        float[] values = new float[9];
-                        matrix.getValues(values);
-                        // values[2] and values[5] are the x,y coordinates of the top left corner of the drawable image, regardless of the zoom factor.
-                        // values[0] and values[4] are the zoom factors for the image's width and height respectively. If you zoom at the same factor, these should both be the same value.
-                        float relativeX = (event.getX() - values[2]) / values[0];
-                        float relativeY = (event.getY() - values[5]) / values[4];
-                        Log.d(LOG_TAG, "Relative: x: " + relativeX + " y: " + relativeY);
-                        // values[2] and values[5] are the x,y coordinates of the top left corner of the drawable image, regardless of the zoom factor.
-                        checkClick(relativeX, relativeY);
-                    } else {
-
-                    }
-                }
-                mode = NONE;
-                // Uppdatera mapen...
-                break;
-
-            case MotionEvent.ACTION_POINTER_DOWN: // first and second finger down
-
-                oldDist = spacing(event);
-                newDist = oldDist;
-                if (oldDist > 5f) {
-                    savedMatrix.set(matrix);
-                    midPoint(mid, event);
-                    mode = ZOOM;
-                }
-                break;
-
-            case MotionEvent.ACTION_MOVE:
-
-                if (mode == DRAG) {
-                    matrix.postTranslate(event.getX() - start.x, event.getY() - start.y); // create the transformation in the matrix  of points
-                } else if (mode == ZOOM) {
-                    // pinch zooming
-                    float newDist2 = spacing(event);
-                    // Lock zoom out
-                    if (this.scale * newDist2 / oldDist >= 1) {
-                        //newDist = newDist2;
-                        newDist = newDist2;
-                        if (newDist > 5f) {
-
-                            scale = newDist / oldDist;
-                            // setting the scaling of the
-                            // matrix...if scale > 1 means
-                            // zoom in...if scale < 1 means
-                            // zoom out
-                            matrix.postScale(scale, scale, mid.x, mid.y);
-                        }
-                    }
-                }
-                break;
+    public boolean onTouchEvent(MotionEvent ev) {
+        // Let the ScaleGestureDetector inspect all events first.
+        mScaleDetector.onTouchEvent(ev);
+        if(!mScaleDetector.isInProgress()){
+            mGestures.onTouchEvent(ev);
         }
         invalidate();
-        return true; // indicate event was handled
+        return true;
     }
 
+    private void midPoint(PointF point, MotionEvent event) {
+        float x = event.getX(0) + event.getX(1);
+        float y = event.getY(0) + event.getY(1);
+        point.set(x / 2, y / 2);
+    }
 
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScaleBegin(ScaleGestureDetector detector) {
+            lastFocusX = detector.getFocusX();
+            lastFocusY = detector.getFocusY();
+            return true;
+        }
+
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            mScaleFactor *= Math.max(detector.getScaleFactor(), 1.05f);
+            // Don't let the object get too small or too large.
+            mScaleFactor = Math.max(0.9f, Math.min(mScaleFactor, 100.0f));
+
+            Matrix transformationMatrix = new Matrix();
+            float focusX = detector.getFocusX();
+            float focusY = detector.getFocusY();
+
+            //Zoom focus is where the fingers are centered,
+            transformationMatrix.postTranslate(-focusX, -focusY);
+            transformationMatrix.postScale(mScaleFactor*0.5f, mScaleFactor*0.5f);
+
+            /* Adding focus shift to allow for scrolling with two pointers down. Remove it to skip this functionality. This could be done in fewer lines, but for clarity I do it this way here */
+            //Edited after comment by chochim
+            float focusShiftX = focusX - lastFocusX;
+            float focusShiftY = focusY - lastFocusY;
+            transformationMatrix.postTranslate(focusX + focusShiftX, focusY + focusShiftY);
+            matrix.postConcat(transformationMatrix);
+            lastFocusX = focusX;
+            lastFocusY = focusY;
+            return true;
+        }
+    }
+
+    private class GestureListener implements GestureDetector.OnGestureListener {
+        @Override
+        public boolean onScroll(MotionEvent downEvent, MotionEvent currentEvent, float distanceX, float distanceY) {
+            Log.d(LOG_TAG, "onScroll()");
+            matrix.postTranslate(-distanceX, -distanceY);
+            return true;
+        }
+
+        @Override
+        public void onLongPress(MotionEvent e) {
+            Log.d(LOG_TAG, "onLongPress()");
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            Log.d(LOG_TAG, "onFling()");
+            return false;
+        }
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+            return false;
+        }
+
+        @Override
+        public void onShowPress(MotionEvent e) {
+            Log.d(LOG_TAG, "onShowPress()");
+        }
+
+        @Override
+        public boolean onSingleTapUp(MotionEvent e) {
+            return false;
+        }
+    }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         timer.reset();
+
         canvas.setMatrix(matrix);
         canvas.getClipBounds(r);
         canvas.drawPicture(pic);
-        timer.tick(LOG_TAG, "draw(): " + r);
-    }
 
-    /**
-     * Calculate the space between the two fingers on touch
-     */
-    private float spacing(MotionEvent event) {
-        float x = event.getX(0) - event.getX(1);
-        float y = event.getY(0) - event.getY(1);
-        return (float) Math.sqrt(x * x + y * y);
-    }
-
-    /**
-     * Calculates the midpoint between the two fingers
-     */
-    private void midPoint(PointF point, MotionEvent event) {
-        float x = event.getX(0) + event.getX(1);
-        float y = event.getY(0) + event.getY(1);
-        point.set(x / 2, y / 2);
+        timer.tick(LOG_TAG, "draw(): " + r+ "scale: "+mScaleFactor);
     }
 
     private void checkClick(float relativeX, float relativeY) {
@@ -188,7 +184,8 @@ public class SVGMapView extends View implements View.OnTouchListener {
         Timer t = new Timer();
         this.pic = svg.renderToPicture();
         float initScale = getWidth() * 1.0f / pic.getWidth();
-        this.matrix.setScale(initScale, initScale);
+        this.mScaleFactor = initScale;
+
         t.tick(LOG_TAG, "renderToPicture("+ w +" ,"+ h +")");
         postInvalidate();
     }
@@ -196,6 +193,7 @@ public class SVGMapView extends View implements View.OnTouchListener {
     public void setMatrixValues(float[] floatArray) {
         matrix.setValues(floatArray);
     }
+
     public float[] getMatrixValues(){
         float[] floats = new float[9];
         matrix.getValues(floats);
