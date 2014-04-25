f207d03ba05b4d06363fe9f60a194a869b59a6dc
Filip Lindqvist <buggfille@gmail.com>
2014-04-25 12:09:15 +0200

    Implemented preloading of image

--- futural-extern-android/src/main/java/se/lundakarnevalen/extern/android/SplashScreenActivity.java
+++ futural-extern-android/src/main/java/se/lundakarnevalen/extern/android/SplashScreenActivity.java
@@ -5,12 +5,19 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Handler;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.ImageView;
+
+import java.util.concurrent.ExecutionException;
+
+import se.lundakarnevalen.extern.fragments.MapFragment;
 
 import static se.lundakarnevalen.extern.util.ViewUtil.*;
 
 public class SplashScreenActivity extends Activity {
+    private static final String LOG_TAG = SplashScreenActivity.class.getSimpleName();
+
     private static final int SPLASH_TIME_OUT = 300;
 
     @Override
@@ -31,12 +38,15 @@ public class SplashScreenActivity extends Activity {
         }, SPLASH_TIME_OUT);
 
         if(Build.VERSION.SDK_INT >= 11){
-            find(R.id.imgLogo, ImageView.class).animate()
+            find(R.id.imgLogo, ImageView.class)
+                .animate()
                 .rotationBy(360)
                 .setDuration(SPLASH_TIME_OUT)
                 .setInterpolator(new AccelerateDecelerateInterpolator())
                 .start();
         }
+
+        MapFragment.preload(this);
     }
 
     public <T> T find(int id, Class<T> clz) {
