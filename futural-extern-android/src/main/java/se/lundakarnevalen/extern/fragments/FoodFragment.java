9368dee25cbd9efcd08b75f59d7253a26e5ddb50
Fredrik Persson <fredrik-persson@outlook.com>
2014-05-04 22:00:10 +0200

    Moved all data to a separate class

--- futural-extern-android/src/main/java/se/lundakarnevalen/extern/fragments/FoodFragment.java
+++ futural-extern-android/src/main/java/se/lundakarnevalen/extern/fragments/FoodFragment.java
@@ -2,19 +2,18 @@ package se.lundakarnevalen.extern.fragments;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
-import se.lundakarnevalen.extern.android.ContentActivity;
 import se.lundakarnevalen.extern.android.R;
+import se.lundakarnevalen.extern.data.DataContainer;
+import se.lundakarnevalen.extern.data.DataElement;
+import se.lundakarnevalen.extern.data.DataType;
 import se.lundakarnevalen.extern.widget.LKListAdapter;
-import se.lundakarnevalen.extern.widget.LKListElement;
-import se.lundakarnevalen.extern.widget.LKListElementType;
 import se.lundakarnevalen.extern.widget.LKListRow;
 
 /**
@@ -23,17 +22,13 @@ import se.lundakarnevalen.extern.widget.LKListRow;
 public class FoodFragment extends LKFragment {
 
 
-    private ArrayList<LKListElement> food = new ArrayList<LKListElement>();
+    private List<DataElement> food = DataContainer.getDataOfType(DataType.FOOD);
 
     // Every time you switch to this fragment.
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_food, null);
         ListView lv = (ListView) rootView.findViewById(R.id.fragment_food_list);
-
-        if (food.isEmpty()) {
-            addAllFood();
-        }
 
 /*
         lv.setAdapter(new SimpleAdapter(getContext(),
@@ -64,97 +59,6 @@ public class FoodFragment extends LKFragment {
         lv.setAdapter(new LKListAdapter(getContext(), items,getActivity()));
 
         return rootView;
-    }
-
-    private void addAllFood() {
-        food.add(new LKListElement(
-                getString(R.string.cocktail_place),
-                getString(R.string.cocktail_title),
-                getString(R.string.cocktail_info),
-                55.706362f, 13.195165f,
-                R.drawable.monk,
-                R.drawable.nagilima_logo,
-                getString(R.string.cocktail_question),
-                "15:30-22:30","15:30-23:30","15:30-22:30",
-                LKListElementType.FOOD
-                ));
-
-        food.add(new LKListElement(
-                getString(R.string.hipp_baren_place),
-                getString(R.string.hipp_baren_title),
-                getString(R.string.hipp_baren_info),
-                55.706521f, 13.195431f,
-                R.drawable.monk,
-                R.drawable.nagilima_logo,
-                getString(R.string.hipp_baren_question),
-                "15:30-22:30","15:30-23:30","15:30-22:30",
-                LKListElementType.FOOD));
-
-
-        food.add(new LKListElement(
-                getString(R.string.folkan_place),
-                getString(R.string.folkan_title),
-                getString(R.string.folkan_info),
-                55.706841f, 13.196030f,
-                R.drawable.monk,
-                R.drawable.nagilima_logo,
-                getString(R.string.folkan_question),
-                "15:30-22:30","15:30-23:30","15:30-22:30",
-                LKListElementType.FOOD));
-
-        ArrayList<String> menu = new ArrayList<String>();
-        ArrayList<String> menuPrice = new ArrayList<String>();
-        menu.add(getString(R.string.undervatten_food1));
-        menu.add(getString(R.string.undervatten_food2));
-        menu.add(getString(R.string.undervatten_food3));
-     //   menu.add("");
-        menuPrice.add("??");
-        menuPrice.add("??");
-        menuPrice.add("??");
-
-        food.add(new LKListElement(
-                getString(R.string.krog_undervatten_place),
-                getString(R.string.krog_undervatten_title),
-                getString(R.string.krog_undervatten_info),
-                55.705154f, 13.19458f,
-                R.drawable.monk,
-                R.drawable.undervatten_logo,
-                getString(R.string.krog_undervatten_question),
-                "15:30-22:30","15:30-23:30","15:30-22:30",
-                LKListElementType.FOOD, menu, menuPrice));
-
-        food.add(new LKListElement(
-                getString(R.string.krog_lajka_place),
-                getString(R.string.krog_lajka_title),
-                getString(R.string.krog_lajka_info),
-                55.705697f, 13.194630f,
-                R.drawable.monk,
-                R.drawable.undervatten_logo,
-                getString(R.string.krog_lajka_question),
-                "12:00-01:00","12:00-01:00","12:00-24:00",
-                LKListElementType.FOOD));
-        menu = new ArrayList<String>();
-        menuPrice = new ArrayList<String>();
-        menu.add(getString(R.string.thyme_food1));
-        menu.add(getString(R.string.thyme_food2));
-        //   menu.add("");
-        menuPrice.add("??");
-        menuPrice.add("??");
-
-
-        food.add(new LKListElement(
-                getString(R.string.krog_thyme_place),
-                getString(R.string.krog_thyme_title),
-                getString(R.string.krog_lajka_info),
-                55.705697f, 13.194630f,
-                R.drawable.monk,
-                R.drawable.fine_dine_logo,
-                getString(R.string.krog_thyme_question),
-                "14:00-02:00","14:00-02:00","14:00-02:00",
-                LKListElementType.FOOD,
-                menu, menuPrice));
-
-
     }
 
 
