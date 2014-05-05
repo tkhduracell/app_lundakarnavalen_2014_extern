82fcf0f8319ea1d87e98a56c1076fccfdc616cca
Fredrik Persson <fredrik-persson@outlook.com>
2014-05-05 21:45:21 +0200

    Fixed conflict with RADIO enum

--- futural-extern-android/src/main/java/se/lundakarnevalen/extern/data/DataContainer.java
+++ futural-extern-android/src/main/java/se/lundakarnevalen/extern/data/DataContainer.java
@@ -1,7 +1,5 @@
 package se.lundakarnevalen.extern.data;
 
-import android.text.Html;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -194,7 +192,7 @@ public class DataContainer {
         data.add(new DataElement(
                 R.string.karne_name,
                 R.drawable.music_logo,
-                DataType.FUTURAL));
+                DataType.PLAYER_FUTURAL));
         data.add(new DataElement(
                 R.string.train,
                 R.drawable.train_logo_other,
@@ -202,7 +200,7 @@ public class DataContainer {
         data.add(new DataElement(
                 R.string.radio_title,
                 R.drawable.radio_logo,
-                DataType.RADIO));
+                DataType.PLAYER_RADIO));
         data.add(new DataElement(
                 R.string.toilets_title,
                 R.drawable.toilets_logo,
@@ -285,7 +283,21 @@ public class DataContainer {
         data.add(new DataElement(R.string.entre, R.string.intergalaktisk_invandring, 55.7037410100921f, 13.19423114265037f, R.drawable.radio_logo, DataType.ENTRANCE));
         data.add(new DataElement(R.string.entre, R.string.ett_svart_hal, 55.7044690873277f, 13.19560129896877f, R.drawable.radio_logo, DataType.ENTRANCE));
 
-        
+        data.add(new DataElement(R.string.snacks, R.string.discokaffetalt, 55.70519298937666f, 13.19317860493478f, R.drawable.snaxeriet_logo, DataType.SNACKS));
+        data.add(new DataElement(R.string.snacks, R.string.snacks_iv, 55.70585795050108f, 13.1943144945812f, R.drawable.snaxeriet_logo, DataType.SNACKS));
+        data.add(new DataElement(R.string.snacks, R.string.lyxbrygghus, 55.70547249304708f, 13.19451161073117f, R.drawable.snaxeriet_logo, DataType.SNACKS));
+        data.add(new DataElement(R.string.snacks, R.string.lyxbrygghus, 55.70479839152454f, 13.19472215029551f, R.drawable.snaxeriet_logo, DataType.SNACKS));
+        data.add(new DataElement(R.string.snacks, R.string.kungliga_snacksboden_estrelle, 55.70520990752672f, 13.19387434532636f, R.drawable.snaxeriet_logo, DataType.SNACKS));
+        data.add(new DataElement(R.string.snacks, R.string.bacon_oriley, 55.70642208831894f, 13.1957497314886f, R.drawable.snaxeriet_logo, DataType.SNACKS));
+
+        data.add(new DataElement(R.string.tombolan, R.string.putins_bodega_put_in_putin, 55.70652897496141f, 13.19567109412686f, R.drawable.bubble_tombolan, DataType.TOMBOLAN));
+        data.add(new DataElement(R.string.tombolan, R.string.rysk_roulette, 55.70598296464737f, 13.19420818471242f, R.drawable.bubble_tombolan, DataType.TOMBOLAN));
+        data.add(new DataElement(R.string.tombolan, R.string.hipster_eller_hobo, 55.70597691961526f, 13.19425780556136f, R.drawable.bubble_tombolan, DataType.TOMBOLAN));
+        data.add(new DataElement(R.string.tombolan, R.string.ligga_i_lund, 55.7052623716287f, 13.1933726994219f, R.drawable.bubble_tombolan, DataType.TOMBOLAN));
+        data.add(new DataElement(R.string.tombolan, R.string.war_of_karnewall_street, 55.70497261402468f, 13.19400101442841f, R.drawable.bubble_tombolan, DataType.TOMBOLAN));
+        data.add(new DataElement(R.string.tombolan, R.string.tillbaka_till_samtiden, 55.7046899767097f, 13.19470679837929f, R.drawable.bubble_tombolan, DataType.TOMBOLAN));
+        data.add(new DataElement(R.string.tombolan, R.string.strike_for_the_future, 55.7045492497648f, 13.19459512816178f, R.drawable.bubble_tombolan, DataType.TOMBOLAN));
+        data.add(new DataElement(R.string.tombolan, R.string.futuralbucks, 55.7046234675636f, 13.19465534628458f, R.drawable.bubble_tombolan, DataType.TOMBOLAN));
 
         return data;
     }
