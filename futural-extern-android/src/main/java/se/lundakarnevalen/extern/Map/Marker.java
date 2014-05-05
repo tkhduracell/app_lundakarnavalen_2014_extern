b5972cf62d84c8d64e2ace3863ccf85914198495
Filip Lindqvist <buggfille@gmail.com>
2014-05-05 23:06:57 +0200

    Made DataElement backend to marker, fixed zoom and select issue

--- futural-extern-android/src/main/java/se/lundakarnevalen/extern/Map/Marker.java
+++ futural-extern-android/src/main/java/se/lundakarnevalen/extern/Map/Marker.java
@@ -1,48 +1,32 @@
 package se.lundakarnevalen.extern.map;
 
-import android.util.Log;
-
 import se.lundakarnevalen.extern.data.DataElement;
-import se.lundakarnevalen.extern.data.DataType;
 
 /**
  * Created by Filip on 2014-04-18.
  */
 public class Marker {
-    public static final float CLOSE_THRESHOLD = 240.0f;
     public static final String LOG_TAG = "Marker";
 
     public float x = -1;
     public float y = -1;
 
-    public float lat;
-    public float lng;
-
     public int picture;
-    public DataType type;
+    public DataElement element;
 
     public boolean isFocusedInMap = false;
 
     public Marker(DataElement elm ) {
-        this.lat = elm.lat;
-        this.lng = elm.lng;
-        this.type = elm.type;
+        this.element = elm;
         this.picture = elm.picture;
     }
 
-    public Marker(float v, float v1, int barnevalen_logo, int fun) {
+    public Marker(float v, float v1, int barnevalen_logo, int fun) {}
 
-    }
-
-    public boolean isClose(float relativeX, float relativeY) {
+    public float distance(float relativeX, float relativeY) {
         float dx = x - relativeX;
-        float dy = y - relativeY - 20;
-        float dist = dx * dx + dy * dy;
-        if (dist < CLOSE_THRESHOLD) {
-            Log.d(LOG_TAG, "isClose: dist="+dist);
-            return true;
-        }
-        return false;
+        float dy = y - relativeY;
+        return dx * dx + dy * dy;
     }
 
 }
