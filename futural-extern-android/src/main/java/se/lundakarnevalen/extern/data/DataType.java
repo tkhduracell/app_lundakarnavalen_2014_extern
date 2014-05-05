f468d95c61db3f8ec65eb82d143274bcd7d12c3b
Fredrik Persson <fredrik-persson@outlook.com>
2014-05-05 21:22:46 +0200

    Added more data points

--- futural-extern-android/src/main/java/se/lundakarnevalen/extern/data/DataType.java
+++ futural-extern-android/src/main/java/se/lundakarnevalen/extern/data/DataType.java
@@ -4,4 +4,4 @@ package se.lundakarnevalen.extern.data;
  * Created by Fredrik on 2014-05-04.
  */
 public enum DataType { FOOD, FUN, OTHER, RADIO, FUTURAL,
-    TRAIN, SMALL_FUN, TENT_FUN, BARNEVALEN, TOILETS, CARE }
+    TRAIN, SMALL_FUN, TENT_FUN, BARNEVALEN, TOILETS, CARE, BILJETTERIET, POLICE, RADIO }
