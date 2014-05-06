02a34da1f76e28e97ddd39d2ff0ea40c5bd09cf5
Markus <karl.markus.larsson@gmail.com>
2014-05-06 11:30:33 +0200

    Update notifications!

--- futural-extern-android/src/main/java/se/lundakarnevalen/extern/scheme/Event.java
+++ futural-extern-android/src/main/java/se/lundakarnevalen/extern/scheme/Event.java
@@ -11,13 +11,15 @@ public class Event {
     public int image;
     public Date startDate;
     public Date endDate;
+    public int id;
 
-    public Event(String place, String title, int image, Date startDate,Date endDate) {
+    public Event(String place, String title, int image, Date startDate,Date endDate,int id) {
         this.endDate = endDate;
         this.place = place;
         this.title = title;
         this.image = image;
         this.startDate = startDate;
+        this.id = id;
     }
 
 
--- futural-extern-android/src/main/java/se/lundakarnevalen/extern/util/SchemeAlarm.java
+++ futural-extern-android/src/main/java/se/lundakarnevalen/extern/util/SchemeAlarm.java
@@ -11,6 +11,8 @@ import android.util.Log;
 
 import se.lundakarnevalen.extern.android.ContentActivity;
 import se.lundakarnevalen.extern.android.R;
+import se.lundakarnevalen.extern.android.SplashScreenActivity;
+import se.lundakarnevalen.extern.fragments.SchemeFragment;
 
 /**
  * Created by Markus on 2014-05-05.
@@ -19,19 +21,23 @@ public class SchemeAlarm extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d("hej alarm","ALARM");
         NotificationManager mNM;
         mNM = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
         // Set the icon, scrolling text and timestamp
-        Notification notification = new Notification(R.drawable.splash, "",
+        Notification notification = new Notification(R.drawable.splash, "LUNDAKARNEVALEN",
                 System.currentTimeMillis());
         // The PendingIntent to launch our activity if the user selects this notification
-        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, new Intent(context, ContentActivity.class), 0);
+        // TODO Bitmap crash!
+        Intent i = new Intent(context, ContentActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, i, 0);
         // Set the info for the views that show in the notification panel.
 
         notification.setLatestEventInfo(context, intent.getStringExtra("Title"), intent.getStringExtra("Desc"), contentIntent);
         notification.sound = Uri.parse("android.resource://" + context.getPackageName() + "/" + R.raw.futufutu);
         notification.flags |= Notification.FLAG_AUTO_CANCEL;
+
+
         // Send the notification.
         // We use a layout id because it is a unique number. We use it later to cancel.
         mNM.notify(intent.getStringExtra("Title") + intent.getStringExtra("Desc"),0, notification);
