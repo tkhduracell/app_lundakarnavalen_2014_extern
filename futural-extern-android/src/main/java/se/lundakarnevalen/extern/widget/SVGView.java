fde70b970695be307474d587f9e71d5a62bfe6b3
Filip Lindqvist <buggfille@gmail.com>
2014-05-07 02:50:51 +0200

    Modified entrance icon, only paints bubbles inside of viewport

--- futural-extern-android/src/main/java/se/lundakarnevalen/extern/widget/SVGView.java
+++ futural-extern-android/src/main/java/se/lundakarnevalen/extern/widget/SVGView.java
@@ -6,6 +6,7 @@ import android.graphics.Matrix;
 import android.graphics.Picture;
 import android.graphics.PointF;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -50,7 +51,6 @@ public class SVGView extends View {
     protected float mMinZoom;
     protected Matrix mMatrix;
     protected Picture mPicture;
-    protected Rect mCurrentViewBound;
 
     protected final float[] mPictureEndPoint = new float[]{-1f, -1f};
     protected final float[] mViewEndPoint = new float[]{-1f, -1f};
@@ -75,7 +75,6 @@ public class SVGView extends View {
         mMatrix = new Matrix();
         mSavedMatrix = new Matrix();
         mInverseMatrix = new Matrix();
-        mCurrentViewBound = new Rect();
         if(!isInEditMode()){
             mPicture = new Picture();
             mScaleDetector = new ScaleGestureDetector(context, new ScaleListener());
@@ -189,6 +188,13 @@ public class SVGView extends View {
         return pts;
     }
 
+    public RectF getCurrentViewPort(RectF dst){
+        mMatrix.invert(mInverseMatrix);
+        dst.set(0,0, mViewEndPoint[0], mViewEndPoint[1]);
+        mInverseMatrix.mapRect(dst);
+        return dst;
+    }
+
     private class GestureListener implements GestureDetector.OnGestureListener {
         @Override
         public boolean onScroll(MotionEvent downEvent, MotionEvent currentEvent, float distanceX, float distanceY) {
@@ -197,7 +203,9 @@ public class SVGView extends View {
         }
 
         @Override
-        public void onLongPress(MotionEvent e) {}
+        public void onLongPress(MotionEvent e) {
+            Log.d(LOG_TAG, "CurrentViewPort: " + getCurrentViewPort(new RectF(0,0,0,0)).toShortString());
+        }
 
         @Override
         public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {return false;}
@@ -263,7 +271,6 @@ public class SVGView extends View {
         //canvas.save();
         filterMatrix(mMatrix);
         canvas.setMatrix(mMatrix);
-        canvas.getClipBounds(mCurrentViewBound);
         onDrawObjects(canvas);
         //canvas.restore();
         acc += System.currentTimeMillis() - t0;
