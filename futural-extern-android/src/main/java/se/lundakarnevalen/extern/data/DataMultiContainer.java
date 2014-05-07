73b5088cad60f96085305978ac796c27fdb2c486
Markus <karl.markus.larsson@gmail.com>
2014-05-07 23:19:09 +0200

    fix pictures for the fun

--- futural-extern-android/src/main/java/se/lundakarnevalen/extern/data/DataMultiContainer.java
+++ futural-extern-android/src/main/java/se/lundakarnevalen/extern/data/DataMultiContainer.java
@@ -15,40 +15,36 @@ public class DataMultiContainer {
 
         data.add(new DataElement(
                 R.string.smanojen,
-                R.string.smanojen_place,
                 R.string.smanojen_question,
                 R.string.smanojen_info,
                 1f,1f,
-                R.drawable.header_showen,
+                R.drawable.header_other,
                 R.drawable.smanojen_logo_list,
                 DataType.SMALL_FUN));
 
         data.add(new DataElement(
                 R.string.taltnojen,
-                R.string.taltnojen_place,
                 R.string.taltnojen_question,
                 R.string.taltnojen_info,
                 1f,1f,
                 R.drawable.header_showen,
-                R.drawable.taltnojen_logo,
+                R.drawable.tent_logo_list,
                 DataType.TENT_FUN));
         data.add(new DataElement(
                 R.string.tombolan,
-                R.string.tombolan_place,
                 R.string.tombolan_question,
                 R.string.tombolan_info,
                 1f,1f,
                 R.drawable.header_tombolan,
-                R.drawable.bubble_tombolan,
+                R.drawable.tombolan_logo_list,
                 DataType.TOMBOLAN));
         data.add(new DataElement(
                 R.string.musik,
-                R.string.musik_place,
                 R.string.musik_question,
                 R.string.musik_info,
                 1f,1f,
-                R.drawable.header_tombolan,
-                R.drawable.music_logo,
+                R.drawable.header_kabaren,
+                R.drawable.music_logo_list,
                 DataType.MUSIC));
 
         return data;
