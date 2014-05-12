ac3445d206714dea93f0a4ae5fc6b376538e348c
Filip Lindqvist <ada10fl2@student.lu.se>
2014-05-12 18:03:00 +0200

    Removed the Sempahore implmentation in loader, removed debug manifest

--- futural-extern-android/src/main/java/se/lundakarnevalen/extern/Map/MapLoader.java
+++ futural-extern-android/src/main/java/se/lundakarnevalen/extern/Map/MapLoader.java
@@ -9,6 +9,7 @@ import com.caverock.androidsvg.SVG;
 import com.caverock.androidsvg.SVGParseException;
 
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 
 import se.lundakarnevalen.extern.android.R;
 import se.lundakarnevalen.extern.util.Timer;
@@ -18,9 +19,6 @@ import se.lundakarnevalen.extern.util.Timer;
 */
 public class MapLoader {
     public static final String LOG_TAG = MapLoader.class.getSimpleName();
-
-    private static Semaphore mapSignal = new Semaphore(2);
-    private static Semaphore mapWaitSignal = new Semaphore(1);
 
     private static boolean isLoading = false;
     private static Picture mapMini = null;
@@ -40,10 +38,8 @@ public class MapLoader {
         @Override
         protected Void doInBackground(Void... params) {
             try {
-                mapSignal.acquire(2);
-                isLoading = true;
                 Timer t = new Timer();
-
+                isLoading = true;
                 SVG svg1 = SVG.getFromResource(mContext, R.raw.karta_mini);
                 t.tick(LOG_TAG, "MapMini: getFromResource()");
                 mapMini = svg1.renderToPicture();
@@ -56,13 +52,11 @@ public class MapLoader {
                 t.tick(LOG_TAG, "MapLarge: getFromResource()");
                 mapLarge = svg2.renderToPicture();
                 t.tick(LOG_TAG, "MapLarge: renderToPicture()");
-                mapSignal.release();
 
-                isLoading = false;
             } catch (SVGParseException e) {
                 Log.wtf(LOG_TAG, "This wont happen");
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            } finally {
+                isLoading = false;
             }
             return null;
         }
@@ -85,27 +79,20 @@ public class MapLoader {
                 @Override
                 protected Void doInBackground(Void... params) {
                     try {
-                        Log.d(LOG_TAG, "mapWaitSignal.acquire()");
-                        mapWaitSignal.acquire();
-                        if(mapMini == null) {
-                            mapSignal.acquire();
-                        }
+                        while (mapMini == null) sleep(300);
                         Log.d(LOG_TAG, "posting MiniMap: "+mapMini);
                         mCallback.postMiniMap(mapMini);
-                        if(mapLarge == null) {
-                            mapSignal.acquire();
-                        }
+                        while (mapLarge == null) sleep(300);
                         Log.d(LOG_TAG, "posting LargeMap: "+mapMini);
                         mCallback.postLargerMap(mapLarge);
-                        Log.d(LOG_TAG, "mapWaitSignal.release()");
-                        mapWaitSignal.release();
                     } catch (InterruptedException e) {
                         Log.wtf(LOG_TAG, "Future was interrupted", e);
-                    } finally {
-                        if(mapMini == null) mapSignal.release();
-                        if(mapLarge == null) mapSignal.release();
                     }
                     return null;
+                }
+
+                private void sleep(int ms) throws InterruptedException {
+                    TimeUnit.MILLISECONDS.sleep(ms);
                 }
 
                 @Override
