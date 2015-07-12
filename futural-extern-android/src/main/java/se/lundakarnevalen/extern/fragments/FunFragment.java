8af65d8563683e1f9b7daa05859c0e7a44e46de2
Markus <karl.markus.larsson@gmail.com>
2015-07-12 17:59:27 +0200

    Clean fun fragment

--- futural-extern-android/src/main/java/se/lundakarnevalen/extern/fragments/FunFragment.java
+++ futural-extern-android/src/main/java/se/lundakarnevalen/extern/fragments/FunFragment.java
@@ -18,15 +18,11 @@ import se.lundakarnevalen.extern.data.DataType;
 import se.lundakarnevalen.extern.widget.LKListAdapter;
 import se.lundakarnevalen.extern.widget.LKListRow;
 
-/**
- * Created by Markus on 2014-04-16.
- */
 public class FunFragment extends LKFragment {
 
-    private List<DataElement> fun = new ArrayList<DataElement>();
+    private List<DataElement> fun = new ArrayList<>();
     private final int ID = 0;
 
-    // Every time you switch to this fragment.
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ContentActivity activity = ContentActivity.class.cast(getActivity());
@@ -44,23 +40,7 @@ public class FunFragment extends LKFragment {
             fun.addAll(DataMultiContainer.getAllFunMultiData());
         }
 
-        /*
-        lv.setAdapter(new SimpleAdapter(getContext(),
-                new ArrayList<Map<String, String>>() {{
-                    for(final Fun f: fun) {
-                        add(new HashMap<String, String>() {{
-                            put("title", f.title);
-                            put("place", f.place);
-                            put("picture", ""+f.headerPicture);
-                            put("picture", ""+f.headerPicture);
-                        }});
-                    }
-                }}, R.layout.element_lk_list,
-                new String[]{"title", "place", "picture"},
-                new int[]{R.id.text1, R.id.text2, R.id.image}
-        ));
-        */
-        ArrayList<LKListRow> items = new ArrayList<LKListRow>();
+        ArrayList<LKListRow> items = new ArrayList<>();
 
         for(int i = 0;i< fun.size();i=i+2){
             if(i+1<fun.size()) {
@@ -72,47 +52,6 @@ public class FunFragment extends LKFragment {
         }
         lv.setAdapter(new LKListAdapter(getContext(), items,getActivity()));
 
-        /*
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                ContentActivity.class
-                        .cast(getActivity())
-                        .loadFragmentAddingBS(
-                                LandingPageFragment.create(
-                                        fun.get(position).title,
-                                        fun.get(position).place,
-                                        true, true, true,
-                                        fun.get(position).lat,
-                                        fun.get(position).lng,
-                                        fun.get(position).picture,
-                                        fun.get(position).headerPicture,
-                                        fun.get(position).question,
-                                        fun.get(position).info)
-                        );
-            }
-        });
-        */
         return root;
     }
-
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-    }
-
-    private static class FunItem {
-        String text1;
-        String text2;
-
-        private FunItem(String text1, String text2) {
-            this.text1 = text1;
-            this.text2 = text2;
-        }
-    }
-
-
-
 }
